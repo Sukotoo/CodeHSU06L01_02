df896c69e27c952b5fb8bdf4702158fecfb8bc95
Sukotoo <115269003+Sukotoo@users.noreply.github.com>
2023-02-22 11:04:02 -0800

    finished everything i think

--- src/ArrayFun.java
+++ src/ArrayFun.java
@@ -2,31 +2,38 @@ public class ArrayFun {
 
     public static void main(String[] args) {
         //declare an empty double array of size 6
-
+        double[] doubleArray = new double[5];
         //declare an array of 10 ints
-
+        int[] intArray = {1,2,3,4,5,6,7,8,9,10};
         //how are these elements organized?
 
         //getting a value
-
-
+        int fifthElement = intArray[4];
+        System.out.println(fifthElement);
         //replacing a value
-
-
+        intArray[2] = 69;
+        System.out.println(intArray[2]);
         //getting the length of an array
-
-
+        System.out.println("array length: " + intArray.length);
+        System.out.println();
         ///////////////traversing arrays/////////////////
 
         //printing all the values in an array
-
+        System.out.println("While loop:");
         //while loop
+        int myVar = 0;
+        while(myVar < intArray.length)
+        {
+            System.out.println(intArray[myVar]);
+            myVar++;
+        }
 
-
-
-
+        System.out.println("For loop:");
         //for loop
-
+        for (int i = 0; i < intArray.length; i++)
+        {
+            System.out.println(intArray[i]);
+        }
 
 
 
